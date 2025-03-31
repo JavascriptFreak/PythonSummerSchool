29d66fcfa43f6aec739e8a54f708f6834db72e6c
JavascriptFreak <aishwary081997@gmail.com>
2025-03-31 11:07:35 +0200

    New changes add for the first assignment

---
+++ sandbox.py
@@ -0,0 +1,7 @@
+#day-1 bootcamp basics
+print("Hello , Pro-ITD")
+rounded:int=4
+print("normal print without f string")
+print(f"rounded is:{rounded}")
+fraction:float = float(rounded)
+print(f"fraction is:{fraction}")
